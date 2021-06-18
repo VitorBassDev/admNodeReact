3c56b1e8b81bbcf19234ee24e547cc718d25b2e4
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-18 15:21:45 -0300

    Start to Project Created projetct Created Middleware Auth use of jwt library

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+require('dotenv/config')
+app.use(express.json())
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const { eAdmin } = require('./middlewares/auth')
+
+
+app.use((req, res, next) =>{
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header("Access-Control-Allow-Headers" , "X-PINGOTHER, Content-Type', 'Authorization"),
+  app.use(cors())
+  next()
+              
+})
+
+app.get('/usuarios', eAdmin, (req, res) => {
+  return res.json({    
+    erro: false,
+    message: "Listar Usuários"
+  })  
+})
+
+app.post('/login', (req, res) => {
+
+  if(req.body.usuario === 'vitor@teste.com' && req.body.senha === '123456'){
+
+    const {id} = 1
+    var privateKey = process.env.SECRET
+    var token = jwt.sign({id}, privateKey,{
+      expiresIn: '3d' //10min
+    })   
+
+    return res.json({
+      erro: false,
+      message: "Login Válido",
+      dados: req.body,
+      token
+    }) 
+  }
+  return res.json({
+    erro: true,
+    message: "Login Inválido",
+  })  
+})
+
+app.listen(process.env.BACKEND_PORT, () =>{
+  console.log(`Server is Run on PORT`, process.env.BACKEND_PORT)
+})
---
+++ backend/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const {promisify} = require('util')
+require('dotenv/config')
+
+module.exports = {
+
+  // Verificar se o token é válido
+
+  eAdmin: async function (req, res, next){
+    const authHeader = req.headers.authorization
+    const [, token] = authHeader.split(' ')
+  
+    if(!token){
+      return res.json({
+        erro: true,
+        message: "Erro, Necessário realizar o login para acessar a página"
+      })
+    }
+  
+    try {
+        const decode = await promisify(jwt.verify)(token, process.env.SECRET)
+        req.userId = decode.id
+        return next()
+    } catch (err) {
+        return res.json({
+          erro: true,
+          message: "Erro, Login ou senha inválido!"
+        }) 
+    }
+  }
+}
