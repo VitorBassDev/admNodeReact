71bb9906cec1c162845dba4e32ff1e935e1f58a1
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-18 21:35:58 -0300

    Created models to usuarios

---
+++ backend/database/models/Usuarios.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = require('./db.js')
+
+const Usuario = db.define('usuarios', {
+
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  }, 
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password:{
+    type: Sequelize.STRING,
+    allowNull: true
+  }
+})
+
+Usuario.sync();
+
+module.exports = Usuario
