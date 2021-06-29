7fb0670d995ee0d62a101eec236db87faeb4b4a1
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-29 18:45:49 -0300

    Feat: EndPoint - Cadastrar Usuario Cadastro de usuario Senha ainda nao criptografada Busca os valores passados pelo parametro BODY e salva no banco de dados

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ app.use(express.json())
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const { eAdmin } = require('./middlewares/auth') 
-
+const Usuario = require('./database/models/Usuario')
+// const db = require('../backend/database/models/db.js')
 
 app.use((req, res, next) =>{
   res.header("Access-Control-Allow-Origin", "*")
@@ -23,28 +24,60 @@ app.get('/usuarios', eAdmin, (req, res) => {
   })  
 })
 
-app.post('/login', (req, res) => {
+app.post('/usuario', async (req, res) => {
+  var dados = req.body
 
-  if(req.body.usuario === 'vitor@teste.com' && req.body.senha === '123456'){
-
-    const {id} = 1
-    var privateKey = process.env.SECRET
-    var token = jwt.sign({id}, privateKey,{
-      expiresIn: '3d' //10min
-    })   
-
-    return res.json({
+  await Usuario.create(dados).then(function(){
+    return res.json({       
       erro: false,
-      message: "Login Válido",
-      dados: req.body,
-      token
-    }) 
-  }
-  return res.json({
-    erro: true,
-    message: "Login Inválido",
-  })  
+      message: "Usuário cadastrado com Sucesso!"
+    })
+  }).catch(function(){
+    return res.json({    
+      erro: true,
+      message: "Error: Usuário não cadastrado com Sucesso!"
+    })
+  }) 
+
 })
+
+// app.get('/listaUser', async(req, res ) =>{
+//   await Usuario.findAll().then(function(){
+//     return res.json({    
+//       erro: false,
+//       message: "Lista de Usuários"
+//     })
+//   }).catch((err), function(){
+//     console.log(err)
+//     return res.json({    
+//       erro: true,
+//       message: "Error ao Buscar Listas!"
+//     })
+//   }) 
+// })
+
+// app.post('/login', (req, res) => {
+
+//   if(req.body.usuario === 'vitor@teste.com' && req.body.senha === '123456'){
+
+//     const {id} = 1
+//     var privateKey = process.env.SECRET
+//     var token = jwt.sign({id}, privateKey,{
+//       expiresIn: '3d' //10min
+//     })   
+
+//     return res.json({
+//       erro: false,
+//       message: "Login Válido",
+//       dados: req.body,
+//       token
+//     }) 
+//   }
+//   return res.json({
+//     erro: true,
+//     message: "Login Inválido",
+//   })  
+// })
 
 app.listen(process.env.BACKEND_PORT, () =>{
   console.log(`Server is Run on PORT`, process.env.BACKEND_PORT)
