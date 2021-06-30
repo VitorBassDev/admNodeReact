0549002fdd2a74ea0a83fe067fb44783eb061d3e
Vítor Guedes <vittorbassdev@gmail.com>
2021-06-30 15:07:26 -0300

    Feat: Estrutura do projeto frontend Projeto React Estruturação do projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Projeto Sis Adm - React|Node</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
