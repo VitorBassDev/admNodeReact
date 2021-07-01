3822bac3a5d427a8e61f769f3a2b157690ba2c7e
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-01 15:49:23 -0300

    Feat: Pagina de Dashboard Recarrega o token Possui Rota para destruir a sessao

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+
+import { Context } from '../../Context/AuthContext';
+
+export const Dashboard = () => {
+
+  const { handleLogout } = useContext(Context);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+         
+        <button type="button" onClick={handleLogout}>Sair</button>
+      </div>
+    );
+}
