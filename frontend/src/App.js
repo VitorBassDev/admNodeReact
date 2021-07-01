330517320dc57ff66a0b8529d479b926872fa2a3
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-01 15:46:02 -0300

    Feat: Redefinicao das rotas da aplicacao Utilizadaçao da biblioteca history Configuracao de rotas privadas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Login from './pages/Login'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Routes from './routes/routesAdm'
+import history from './services/history'
 import {AuthProvider} from './Context/AuthContext'
 
 function App() {
   return (
     <div>
       <AuthProvider>
-        <Router>
-          <Switch>
-            <Route path="/" component={Login}/>
-          </Switch>
+        <Router history={history}>
+          <Routes/>
         </Router>
       </AuthProvider>
     </div>
---
+++ frontend/src/services/history.js
@@ -0,0 +1,7 @@
+import { createBrowserHistory } from 'history';
+
+export default createBrowserHistory();
+
+
+
+// npm i --save history@4.9.0
