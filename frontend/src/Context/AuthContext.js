7e2ebf0bbc7abeb21a9df543bd11714bc6a97369
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-01 15:47:19 -0300

    Feat: Context para Salvar dados Salvar token Carregar Token Excluir Token

--- frontend/src/Context/AuthContext.js
+++ frontend/src/Context/AuthContext.js
@@ -1,10 +1,48 @@
-import React, {createContext} from 'react'
+import React, {createContext, useEffect, useState} from 'react'
+import history from '../services/history';
+import api from '../services/config/api';
 
 const Context = createContext();
 
 function AuthProvider({children}){
+
+  const [authenticated, setAuthenticated] = useState(false)
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    const getLogin = async()=>{
+
+      const token = localStorage.getItem('token')
+      if(token){
+        api.defaults.headers.Authenticated = `Bearer ${JSON.parse(token)}`
+        setAuthenticated(true)
+      }
+      setLoading(false)
+    }
+    getLogin()
+  }, [])
+
+  if(loading){
+    return <h1>Carregando...</h1>
+  }
+
+  async function signIn(sit){
+    setAuthenticated(sit);
+}
+
+  function handleLogout(){
+    setAuthenticated(false);
+
+    localStorage.removeItem('token');
+
+    api.defaults.headers.Authorization = undefined;
+
+    return history.push('/');
+}
+
   return(
-    <Context.Provider value={{Authenticated: false}}> 
+    <Context.Provider value={{authenticated, handleLogout, signIn}}> 
       {children}
     </Context.Provider>
   )
