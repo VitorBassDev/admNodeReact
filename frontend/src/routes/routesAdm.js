8d05405c72e484e8c26d6e0abcb54b5215a273c6
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-02 16:22:24 -0300

    Feat: Reativaçao da Rota de Vizualizar Usuarios

--- frontend/src/routes/routesAdm.js
+++ frontend/src/routes/routesAdm.js
@@ -6,7 +6,7 @@ import {Context} from '../Context/AuthContext';
 import Login from '../pages/Login'
 import {Dashboard} from '../pages/Dashboard';
 import { Listar } from '../pages/Listar';
-// import { Visualizar } from '../pages/Visualizar';
+import { Visualizar } from '../pages/Visualizar';
 import { Cadastrar } from '../pages/Cadastrar';
 import { Editar } from '../pages/Editar';
 
@@ -27,7 +27,7 @@ export default function RoutesAdm() {
         <CustomRoute exact path="/" component={Login} />
         <CustomRoute isPrivate exact path="/dashboard" component={Dashboard} />
         <CustomRoute isPrivate exact path="/listar" component={Listar} />
-        {/* <CustomRoute isPrivate exact path="/visualizar/:id" component={Visualizar} /> */}
+        <CustomRoute isPrivate exact path="/visualizar/:id" component={Visualizar} />
         <CustomRoute isPrivate exact path="/cadastrar" component={Cadastrar} />
         <CustomRoute isPrivate exact path="/editar/:id" component={Editar} />
       </Switch>
